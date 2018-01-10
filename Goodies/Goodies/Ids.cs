f8bb0431f73a1a999c6d1eae444949f6564af9ac
BusterWood <chris.j.austin@gmail.com>
2018-01-10 18:08:10 +0000

    Rename ids

--- Goodies/Goodies/Ids.cs
+++ Goodies/Goodies/Ids.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusterWood.Ducks;
+using System;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
@@ -9,7 +10,7 @@ namespace BusterWood.Goodies
     /// <typeparam name="TEntity">The type of the ID is for, e.g. Order</typeparam>
     /// <typeparam name="TKey">The type of ID, e.g. int</typeparam>
     /// <remarks>Does NOT support XML serialization</remarks>
-    public struct Id<TEntity, TKey> : IEquatable<Id<TEntity, TKey>>
+    public struct Id<TEntity, TKey> : IEquatable<Id<TEntity, TKey>>, IXmlSerializable
         where TKey : IEquatable<TKey>
     {
         public Id(TKey value)
@@ -17,84 +18,92 @@ namespace BusterWood.Goodies
             Value = value;
         }
 
-        public TKey Value { get; }
+        public TKey Value { get; private set; }
         public override string ToString() => $"{typeof(TEntity)} {Value}";
         public override bool Equals(object obj) => obj is Id<TEntity, TKey> && Equals((Id<TEntity, TKey>)obj);
         public bool Equals(Id<TEntity, TKey> other) => Value.Equals(other.Value);
         public override int GetHashCode() => Value.GetHashCode();
+        XmlSchema IXmlSerializable.GetSchema() => null;
+        void IXmlSerializable.ReadXml(XmlReader reader) { Value = Duck.Cast<IParser<TKey>>(typeof(TKey)).Parse(reader.ReadElementContentAsString()); }
+        void IXmlSerializable.WriteXml(XmlWriter writer) { writer.WriteValue(Value); }
         public static explicit operator TKey(Id<TEntity, TKey> id) => id.Value;
         public static explicit operator Id<TEntity, TKey>(TKey id) => new Id<TEntity, TKey>(id);
         public static bool operator ==(Id<TEntity, TKey> left, Id<TEntity, TKey> right) => left.Equals(right);
         public static bool operator !=(Id<TEntity, TKey> left, Id<TEntity, TKey> right) => !left.Equals(right);
     }
 
+    public interface IParser<T>
+    {
+        T Parse(string text);
+    }
+
     /// <summary>Generic Int32 ID type to prevent different types of IDs being mixed up, e.g. client ID and order ID</summary>
     /// <typeparam name="T">The type of the ID is for, e.g. Order</typeparam>
-    public struct IntId<T> : IEquatable<IntId<T>>, IXmlSerializable
+    public struct Int32<T> : IEquatable<Int32<T>>, IXmlSerializable
     {
-        public IntId(int value)
+        public Int32(int value)
         {
             Value = value;
         }
 
         public int Value { get; private set; }
         public override string ToString() => $"{typeof(T)} {Value}";
-        public override bool Equals(object obj) => obj is IntId<T> && Equals((IntId<T>)obj);
-        public bool Equals(IntId<T> other) => Value == other.Value;
+        public override bool Equals(object obj) => obj is Int32<T> && Equals((Int32<T>)obj);
+        public bool Equals(Int32<T> other) => Value == other.Value;
         public override int GetHashCode() => Value.GetHashCode();
         XmlSchema IXmlSerializable.GetSchema() => null;
         void IXmlSerializable.ReadXml(XmlReader reader) { Value = reader.ReadElementContentAsInt(); }
         void IXmlSerializable.WriteXml(XmlWriter writer) { writer.WriteValue(Value); }
-        public static explicit operator int(IntId<T> id) => id.Value;
-        public static explicit operator IntId<T>(int id) => new IntId<T>(id);
-        public static bool operator ==(IntId<T> left, IntId<T> right) => left.Equals(right);
-        public static bool operator !=(IntId<T> left, IntId<T> right) => !left.Equals(right);
+        public static explicit operator int(Int32<T> id) => id.Value;
+        public static explicit operator Int32<T>(int id) => new Int32<T>(id);
+        public static bool operator ==(Int32<T> left, Int32<T> right) => left.Equals(right);
+        public static bool operator !=(Int32<T> left, Int32<T> right) => !left.Equals(right);
     }
 
     /// <summary>Generic Int64 ID type to prevent different types of IDs being mixed up, e.g. client ID and order ID</summary>
     /// <typeparam name="T">The type of the ID is for, e.g. Order</typeparam>
-    public struct LongId<T> : IEquatable<LongId<T>>, IXmlSerializable
+    public struct Int64<T> : IEquatable<Int64<T>>, IXmlSerializable
     {
-        public LongId(long value)
+        public Int64(long value)
         {
             Value = value;
         }
 
         public long Value { get; private set; }
         public override string ToString() => $"{typeof(T)} {Value}";
-        public override bool Equals(object obj) => obj is LongId<T> && Equals((LongId<T>)obj);
-        public bool Equals(LongId<T> other) => Value == other.Value;
+        public override bool Equals(object obj) => obj is Int64<T> && Equals((Int64<T>)obj);
+        public bool Equals(Int64<T> other) => Value == other.Value;
         public override int GetHashCode() => Value.GetHashCode();
         XmlSchema IXmlSerializable.GetSchema() => null;
         void IXmlSerializable.ReadXml(XmlReader reader) { Value = reader.ReadElementContentAsLong(); }
         void IXmlSerializable.WriteXml(XmlWriter writer) { writer.WriteValue(Value); }
-        public static explicit operator long(LongId<T> id) => id.Value;
-        public static explicit operator LongId<T>(long id) => new LongId<T>(id);
-        public static bool operator ==(LongId<T> left, LongId<T> right) => left.Equals(right);
-        public static bool operator !=(LongId<T> left, LongId<T> right) => !left.Equals(right);
+        public static explicit operator long(Int64<T> id) => id.Value;
+        public static explicit operator Int64<T>(long id) => new Int64<T>(id);
+        public static bool operator ==(Int64<T> left, Int64<T> right) => left.Equals(right);
+        public static bool operator !=(Int64<T> left, Int64<T> right) => !left.Equals(right);
     }
 
     /// <summary>Generic GUID ID type to prevent different types of IDs being mixed up, e.g. client ID and order ID</summary>
     /// <typeparam name="T">The type of the ID is for, e.g. Order</typeparam>
-    public struct GuidId<T> : IEquatable<GuidId<T>>, IXmlSerializable
+    public struct Guid<T> : IEquatable<Guid<T>>, IXmlSerializable
     {
-        public GuidId(Guid value)
+        public Guid(Guid value)
         {
             Value = value;
         }
 
         public Guid Value { get; private set; }
         public override string ToString() => $"{typeof(T)} {Value}";
-        public override bool Equals(object obj) => obj is GuidId<T> && Equals((GuidId<T>)obj);
-        public bool Equals(GuidId<T> other) => Value == other.Value;
+        public override bool Equals(object obj) => obj is Guid<T> && Equals((Guid<T>)obj);
+        public bool Equals(Guid<T> other) => Value == other.Value;
         public override int GetHashCode() => Value.GetHashCode();
         XmlSchema IXmlSerializable.GetSchema() => null;
         void IXmlSerializable.ReadXml(XmlReader reader) { Value = Guid.Parse(reader.ReadElementContentAsString()); }
         void IXmlSerializable.WriteXml(XmlWriter writer) { writer.WriteValue(Value); }
-        public static explicit operator Guid(GuidId<T> id) => id.Value;
-        public static explicit operator GuidId<T>(Guid id) => new GuidId<T>(id);
-        public static bool operator ==(GuidId<T> left, GuidId<T> right) => left.Equals(right);
-        public static bool operator !=(GuidId<T> left, GuidId<T> right) => !left.Equals(right);
+        public static explicit operator Guid(Guid<T> id) => id.Value;
+        public static explicit operator Guid<T>(Guid id) => new Guid<T>(id);
+        public static bool operator ==(Guid<T> left, Guid<T> right) => left.Equals(right);
+        public static bool operator !=(Guid<T> left, Guid<T> right) => !left.Equals(right);
     }
 
 }
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ So much useful & free stuff for .NET, built as a library for .NET Standard 2.0.
 * [BusterWood.Collections](https://github.com/busterwood/Goodies/blob/master/Goodies/Collections/README.md) namespace contains the time and space effecient `UniqueList` which implements both `IList<>` and `ISet<>`.
 * [BusterWood.Ducks](https://github.com/busterwood/Goodies/blob/master/Goodies/Ducks/README.md) namespace contains run-time duck typing.
 * [BusterWood.Equality](https://github.com/busterwood/Goodies/blob/master/Goodies/Equality/README.md) namespace contains run-time creation of equality comparers using property names.
-* `BusterWood.Goodies` namespace contains `Id<,>`, `IntId<>`, etc  to ensure you don't mix up your customer and order identifiers.
+* `BusterWood.Goodies` namespace contains `Int32<>`, `Int64<>` and `Guid<>` structs to ensure you don't mix up your customer and order identifiers.
 * `BusterWood.Logging` namespace contains `Log` static class for structured logging to `Console.Error` (StdErr).
 * [BusterWood.Monies](https://github.com/busterwood/Goodies/blob/master/Goodies/Monies/README.md) namespace contains `Money` struct to ensure that you don't add acciendtly add GBP and USD together.
 * `BusterWood.Restarting` namespace contains `RestartMonitoring` that monitor failures in asychrous processes and restart them on failure (with delay).
