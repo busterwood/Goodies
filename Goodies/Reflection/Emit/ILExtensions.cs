acde2bc9d599a81e0b35e38036bc3975f2f1d8a8
busterwood <chris.j.austin@gmail.com>
2018-01-19 14:24:28 +0000

    Add null checks to ILExtensions

--- Goodies/Reflection/Emit/ILExtensions.cs
+++ Goodies/Reflection/Emit/ILExtensions.cs
@@ -8,23 +8,35 @@ namespace BusterWood.Reflection.Emit
     {
         public static LocalBuilder DeclareLocal<T>(this ILGenerator il)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             return il.DeclareLocal(typeof(T));
         }
 
         public static ILGenerator Add(this ILGenerator il)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Add);
             return il;
         }
 
         public static ILGenerator Xor(this ILGenerator il)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Xor);
             return il;
         }
 
         public static ILGenerator Box(this ILGenerator il, Type type)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
             il.Emit(OpCodes.Box, type);
             return il;
         }
@@ -32,13 +44,19 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Loads "this" onto the stack, i.e. argument zero</summary>
         public static ILGenerator This(this ILGenerator il)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Ldarg_0);
             return il;
         }
 
+        /// <summary>Loads an argument address onto the stack</summary>
         public static ILGenerator ArgAddress(this ILGenerator il, int index)
         {
-            if (index < 0) throw new ArgumentOutOfRangeException(nameof(index), "index cannot be negative");
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index), "index cannot be negative");
 
             if (index <= 255)
                 il.Emit(OpCodes.Ldarga_S, index); // short form, more compact IL
@@ -50,7 +68,10 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Loads an argument onto the stack</summary>
         public static ILGenerator Arg(this ILGenerator il, int index)
         {
-            if (index < 0) throw new ArgumentOutOfRangeException(nameof(index), "index cannot be negative");
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index), "index cannot be negative");
             switch (index)
             {
                 case 0:
@@ -78,6 +99,8 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Goto label is last two arguments on the stack are equal</summary>
         public static ILGenerator IfEqualGoto(this ILGenerator il, Label label)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Beq, label);
             return il;
         }
@@ -85,6 +108,8 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Goto label is last argument on the stack is false</summary>
         public static ILGenerator IfFalseGoto(this ILGenerator il, Label notEqual)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Brfalse, notEqual);
             return il;
         }
@@ -92,6 +117,8 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Goto label is last two arguments on the stack are equal</summary>
         public static ILGenerator IfNotEqualGoto(this ILGenerator il, Label notEqual)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Bne_Un, notEqual);
             return il;
         }
@@ -99,72 +126,141 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Goto label is last argument on the stack is true</summary>
         public static ILGenerator IfTrueGoto(this ILGenerator il, Label label)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Brtrue, label);
             return il;
         }
 
-        public static ILGenerator Call<T>(this ILGenerator il, string method) => il.Call(typeof(T).GetTypeInfo().GetDeclaredMethod(method));
+        public static ILGenerator Call<T>(this ILGenerator il, string method)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+
+            return il.Call(typeof(T).GetTypeInfo().GetDeclaredMethod(method));
+        }
 
         public static ILGenerator Call(this ILGenerator il, MethodInfo method)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+
             il.Emit(OpCodes.Call, method);
             return il;
         }
 
         public static ILGenerator Call(this ILGenerator il, ConstructorInfo ctor)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (ctor == null)
+                throw new ArgumentNullException(nameof(ctor));
+
             il.Emit(OpCodes.Call, ctor);
             return il;
         }
 
-        public static ILGenerator GetPropertyValue<T>(this ILGenerator il, string propName) => il.CallGetProperty(typeof(T).GetTypeInfo().GetDeclaredProperty(propName));
+        public static ILGenerator GetPropertyValue<T>(this ILGenerator il, string propName)
+        {
+            if (propName == null)
+                throw new ArgumentNullException(nameof(propName));
+
+            return il.CallGetProperty(typeof(T).GetTypeInfo().GetDeclaredProperty(propName));
+        }
 
         public static ILGenerator CallGetProperty(this ILGenerator il, PropertyInfo prop)
         {
+            if (prop == null)
+                throw new ArgumentNullException(nameof(prop));
+
             il.CallVirt(prop.GetMethod);
             return il;
         }
 
-        public static ILGenerator CallVirt<T>(this ILGenerator il, string method) => il.CallVirt(typeof(T).GetTypeInfo().GetDeclaredMethod(method));
+        public static ILGenerator CallVirt<T>(this ILGenerator il, string method)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+
+            return il.CallVirt(typeof(T).GetTypeInfo().GetDeclaredMethod(method));
+        }
 
         public static ILGenerator CallVirt(this ILGenerator il, MethodInfo method)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+
             il.Emit(OpCodes.Callvirt, method);
             return il;
         }
 
         public static ILGenerator Cast(this ILGenerator il, Type toType)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (toType == null)
+                throw new ArgumentNullException(nameof(toType));
+
             il.Emit(OpCodes.Castclass, toType);
             return il;
         }
 
         public static ILGenerator New(this ILGenerator il, ConstructorInfo ctor)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (ctor == null)
+                throw new ArgumentNullException(nameof(ctor));
+
             il.Emit(OpCodes.Newobj, ctor);
             return il;
         }
 
         public static ILGenerator Multiply(this ILGenerator il)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Mul);
             return il;
         }
 
         public static ILGenerator LoadAddress(this ILGenerator il, LocalBuilder local)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (local == null)
+                throw new ArgumentNullException(nameof(local));
+
             il.Emit(OpCodes.Ldloca, local);
             return il;
         }
 
         public static ILGenerator LoadAddress(this ILGenerator il, FieldBuilder f)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (f == null)
+                throw new ArgumentNullException(nameof(f));
+
             il.Emit(OpCodes.Ldflda, f);
             return il;
         }
 
         public static ILGenerator Load(this ILGenerator il, string str)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Ldstr, str);
             return il;
         }
@@ -172,6 +268,8 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Loads an integer onto the stack.  Uses optimized code for i between 0 and 8</summary>
         public static ILGenerator Load(this ILGenerator il, int i)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             switch (i)
             {
                 case -1:
@@ -213,6 +311,9 @@ namespace BusterWood.Reflection.Emit
 
         public static ILGenerator Load(this ILGenerator il, Type type)
         {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
             il.Emit(OpCodes.Ldtoken, type);
             il.Call(typeof(Type).GetTypeInfo().GetDeclaredMethod(nameof(Type.GetTypeFromHandle)));
             return il;
@@ -220,6 +321,10 @@ namespace BusterWood.Reflection.Emit
 
         public static ILGenerator Load(this ILGenerator il, LocalBuilder local)
         {
+            if (local == null)
+                throw new ArgumentNullException(nameof(local));
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Ldloc, local);
             return il;
         }
@@ -233,6 +338,10 @@ namespace BusterWood.Reflection.Emit
 
         public static ILGenerator Store(this ILGenerator il, LocalBuilder local)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+            if (local == null)
+                throw new ArgumentNullException(nameof(local));
             il.Emit(OpCodes.Stloc, local);
             return il;
         }
@@ -240,30 +349,52 @@ namespace BusterWood.Reflection.Emit
         /// <summary>Tries to cast the value on the stack to a type, result is the cast value or null</summary>
         public static ILGenerator AsType(this ILGenerator il, Type type)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
             il.Emit(OpCodes.Isinst, type);
             return il;
         }
 
         public static ILGenerator Load(this ILGenerator il, FieldBuilder field)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (field == null)
+                throw new ArgumentNullException(nameof(field));
+
             il.Emit(OpCodes.Ldfld, field);
             return il;
         }
 
         public static ILGenerator Store(this ILGenerator il, FieldBuilder field)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (field == null)
+                throw new ArgumentNullException(nameof(field));
+
             il.Emit(OpCodes.Stfld, field);
             return il;
         }
 
         public static ILGenerator Return(this ILGenerator il)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(OpCodes.Ret);
             return il;
         }
 
         public static ILGenerator Return(this ILGenerator il, bool val)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
             il.Emit(val ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
             il.Emit(OpCodes.Ret);
             return il;
@@ -271,6 +402,12 @@ namespace BusterWood.Reflection.Emit
 
         public static ILGenerator Switch(this ILGenerator il, Label[] lables)
         {
+            if (il == null)
+                throw new ArgumentNullException(nameof(il));
+
+            if (lables == null)
+                throw new ArgumentNullException(nameof(lables));
+
             il.Emit(OpCodes.Switch, lables);
             return il;
         }
