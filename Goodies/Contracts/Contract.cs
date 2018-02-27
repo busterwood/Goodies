690948bfe7943be1d42fbfc6b5eeed7ed28936a9
BusterWood <chris.j.austin@gmail.com>
2018-02-27 14:36:09 +0000

    Update contracts

--- Goodies/Contracts/Contract.cs
+++ Goodies/Contracts/Contract.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace BusterWood.Contracts
 {
@@ -7,43 +9,74 @@ namespace BusterWood.Contracts
     /// </summary>
     public static class Contract
     {
-        public static void Requires(bool condition, string message = null)
+        private const string PreConditionFailed = "Pre-condition failed";
+        private const string PostConditionFailed = "Post-condition failed";
+        private const string AssertionFailed = "Assertion failed";
+
+        public static void Requires(bool condition, string message = null, [CallerFilePath] string filePath = null, [CallerMemberName] string member = null, [CallerLineNumber] int line = 0)
         {
             if (!condition)
             {
-                throw new ArgumentException("Pre-condition failed: " + message);
+                var msg = $"{message ?? PreConditionFailed} at {filePath}:{member}:{line}";
+#if DEBUG
+                if (Debugger.IsAttached)
+                    Debugger.Break();
+#endif
+                throw new ArgumentException(msg);
             }
         }
 
-        public static void RequiresNotNull<T>(T obj, string message = null) where T : class
+        public static void RequiresNotNull<T>(T obj, string message = null, [CallerFilePath] string filePath = null, [CallerMemberName] string member = null, [CallerLineNumber] int line = 0) where T : class
         {
             if (obj == null)
             {
-                throw new ArgumentNullException("Pre-condition failed: " + message, (Exception)null);
+                var msg = $"{message ?? PreConditionFailed} at {filePath}:{member}:{line}";
+#if DEBUG
+                if (Debugger.IsAttached)
+                    Debugger.Break();
+#endif
+                throw new ArgumentNullException(msg, (Exception)null);
             }
         }
 
-        public static void RequiresNotNull<T>(T? val, string message = null) where T : struct
+        public static void RequiresNotNull<T>(T? val, string message = null, [CallerFilePath] string filePath = null, [CallerMemberName] string member = null, [CallerLineNumber] int line = 0) where T : struct
         {
             if (val == null)
             {
+                var msg = $"{message ?? PreConditionFailed} at {filePath}:{member}:{line}";
+#if DEBUG
+                if (Debugger.IsAttached)
+                    Debugger.Break();
+#endif
                 throw new ArgumentNullException("Pre-condition failed: " + message, (Exception)null);
             }
         }
 
-        public static void Ensures(bool condition, string message = null)
+        // only for RELEASE builds
+        [Conditional("RELEASE")]
+        public static void Ensures(bool condition, string message = null, [CallerFilePath] string filePath = null, [CallerMemberName] string member = null, [CallerLineNumber] int line = 0)
         {
             if (!condition)
             {
-                throw new InvalidOperationException("Post-condition failed: " + message);
+                var msg = $"{message ?? PostConditionFailed} at {filePath}:{member}:{line}";
+#if DEBUG
+                if (Debugger.IsAttached)
+                    Debugger.Break();
+#endif
+                throw new InvalidOperationException(msg);
             }
         }
 
-        public static void Assert(bool condition, string message = null)
+        public static void Assert(bool condition, string message = null, [CallerFilePath] string filePath = null, [CallerMemberName] string member = null, [CallerLineNumber] int line = 0)
         {
             if (!condition)
             {
-                throw new InvalidOperationException("Assertion failed: " + message);
+                var msg = $"{message ?? AssertionFailed} at {filePath}:{member}:{line}";
+#if DEBUG
+                if (Debugger.IsAttached)
+                    Debugger.Break();
+#endif
+                throw new InvalidOperationException(msg);
             }
         }
     }
