3c348c0964a5c1ea8f0ce2f88dae49154f8f3b52
Christopher Austin <chris.j.austin@gmail.com>
2018-06-26 13:37:07 +0100

    Add datareader extensions

--- Goodies/Data/DataReaderExtensions.cs
+++ Goodies/Data/DataReaderExtensions.cs
@@ -1,5 +1,8 @@
-﻿using BusterWood.Contracts;
+﻿using BusterWood.Collections;
+using BusterWood.Contracts;
 using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 
 namespace BusterWood.Data
@@ -78,5 +81,165 @@ namespace BusterWood.Data
             return reader.IsDBNull(ordinal) ? default(string) : reader.GetString(ordinal);
         }
 
+        public static DbCommand NewStoredProc(this DbConnection connection, string procName)
+        {
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = procName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            return cmd;
+        }
+
+        public static DbCommand NewQuery(this DbConnection connection, string sql)
+        {
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            return cmd;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, bool value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Boolean;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, byte value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Byte;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, short value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Int16;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, int value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Int32;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, long value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Int64;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, decimal value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Decimal;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, float value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Single;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, double value)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = DbType.Double;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, DateTime value, DbType type = DbType.DateTime2)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = type;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        public static DbParameter AddParameter(this DbCommand command, string name, string value, int? length = -1, DbType type = DbType.String)
+        {
+            var p = command.CreateParameter();
+            p.ParameterName = name;
+            p.DbType = type;
+            p.Size = length ?? value?.Length ?? -1;
+            p.Value = value;
+            command.Parameters.Add(p);
+            return p;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="List{T}"/> containing a <typeparamref name="T"/> for each record in the <paramref name="reader"/>
+        /// </summary>
+        public static List<T> ToList<T>(this DbDataReader reader, Func<DbDataReader, T> valueSelector)
+        {
+            var list = new List<T>();
+            while (reader.Read())
+            {
+                var value = valueSelector(reader);
+                list.Add(value);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="ILookup{TKey, TElement}"/> that maps a <typeparamref name="TKey"/> to zero or more <typeparamref name="TValue"/>
+        /// </summary>
+        public static HashLookup<TKey, TValue> ToLookup<TKey, TValue>(this DbDataReader reader, Func<DbDataReader, TKey> keySelector, Func<DbDataReader, TValue> valueSelector)
+        {
+            var lookup = new HashLookup<TKey, TValue>();
+            while (reader.Read())
+            {
+                var key = keySelector(reader);
+                var value = valueSelector(reader);
+                lookup.Add(key, value);
+            }
+            return lookup;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="Dictionary{TKey, TValue}"/> containing a key-value for each row of the input <paramref name="reader"/>
+        /// </summary>
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this DbDataReader reader, Func<DbDataReader, TKey> keySelector, Func<DbDataReader, TKey, TValue> valueSelector)
+        {
+            var map = new Dictionary<TKey, TValue>();
+            while (reader.Read())
+            {
+                var key = keySelector(reader);
+                var value = valueSelector(reader, key);
+                map.Add(key, value);
+            }
+            return map;
+        }
     }
 }
